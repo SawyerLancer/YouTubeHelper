502d5a8c187862b5160cb4aadba98a64683fae51
SawyerLancer <soldier707sawyer@gmail.com>
2019-02-10 04:04:12 +0600

    Img get

--- Test.php
+++ Test.php
@@ -11,6 +11,7 @@ require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer 
 
  $Helper = new YouTubeHelper();
  $Helper->putUrl ('https://www.youtube.com/watch?v=WQZHLA_1umQ&list=OLAK5uy_m7rAtJlnIvt2XqI7P5B5A3iWhhHFgZxEA&index=6');
-echo  $Helper->getUrl ();
+ $array =  $Helper->getImages ();
+ echo  $array['default'];
 
 ?>
--- src/YouTubeHelper.php
+++ src/YouTubeHelper.php
@@ -13,9 +13,19 @@ class YouTubeHelper
     const YOU_TUBE_DEFAULT_LINK = 'https://www.youtube.com/';
     const YOU_TUBE_SHORT_LINK = 'https://youtu.be/';
     const YOU_TUBE_EMBED_LINK = 'https://www.youtube.com/embed/';
+    const YOU_TUBE_IMG_VI = 'https://img.youtube.com/vi/';
     const WATCH_V = 'watch?v=';
     private $message;
     private $str_position;
+    private $img_array = [
+        'default' => null,
+        '0.jpg' => null,
+        '1.jpg' => null,
+        '2.jpg' => null,
+        '3.jpg' => null,
+        '4.jpg' => null,
+        '5.jpg' => null
+    ];
 
     public function putUrl (string $url)
     {
@@ -31,6 +41,26 @@ class YouTubeHelper
     public function getUrl ()
     {
         return $this->urlReplace ();
+    }
+
+    public function getImages()
+    {
+        $this->urlReplace ();
+
+        $temp_link = str_replace(self::YOU_TUBE_EMBED_LINK, self::YOU_TUBE_IMG_VI, $this->url);
+
+        $this->str_position = [
+            'default' => $temp_link.'/default.jpg',
+            '0.jpg' => $temp_link.'/0.jpg',
+            '1.jpg' => $temp_link.'/1.jpg',
+            '2.jpg' => $temp_link.'/2.jpg',
+            '3.jpg' => $temp_link.'/3.jpg',
+            '4.jpg' => $temp_link.'/4.jpg',
+            '5.jpg' => $temp_link.'/5.jpg'
+        ];
+
+        return $this->str_position;
+
     }
 
     private function Init ($url)
