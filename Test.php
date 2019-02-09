a5f2d43b2a6cc66b980ca22dad5d6f9fb518c6d8
vagrant <vagrant@homestead>
2019-02-09 13:35:48 +0000

    First commit

---
+++ Test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sawyerlancer
+ * Date: 09.02.2019
+ * Time: 19:28
+ */
+
+require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer autoload
+
+
+echo YouTubeHelper::getMessage ();
+
+?>
---
+++ src/YouTubeHelper.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sawyerlancer
+ * Date: 09.02.2019
+ * Time: 18:57
+ */
+
+class YouTubeHelper
+{
+
+   public static function getMessage(){
+        return "I still work";
+    }
+
+}
